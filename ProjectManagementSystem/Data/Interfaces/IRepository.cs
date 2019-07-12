593d988390ab536527d2e8078feeaac3c2607799
Sourav Singha <sr.sourav39@gmail.com>
2019-07-12 22:04:30 +0600

    Add Primary Repository & Interfaces

--- ProjectManagementSystem/Data/Interfaces/IRepository.cs
+++ ProjectManagementSystem/Data/Interfaces/IRepository.cs
@@ -5,7 +5,23 @@ using System.Threading.Tasks;
 
 namespace ProjectManagementSystem.Data.Interfaces
 {
-    interface IRepository
+   public interface IRepository<T> where T : class
     {
+
+        IEnumerable<T> GetAll();
+
+        IEnumerable<T> Find(Func<T, bool> predicate);
+
+
+
+        T GetById(int id);
+
+        void Create(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        int Count(Func<T, bool> predicate);
     }
 }
--- ProjectManagementSystem/Data/Repository/Repository.cs
+++ ProjectManagementSystem/Data/Repository/Repository.cs
@@ -2,10 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Data.Interfaces;
 
 namespace ProjectManagementSystem.Data.Repository
 {
-    public class Repository
+    public class Repository<T>:IRepository<T> where T:class
     {
+        private readonly AppDbContext Db;
+
+        public Repository(AppDbContext dbContext)
+        {
+           Db=dbContext;
+        }
+        public int Count(Func<T, bool> predicate)
+        {
+            return Db.Set<T>().Where(predicate).Count();
+
+        }
+
+
+        public void Create(T entity)
+        {
+            Db.Add(entity);
+            Save();
+        }
+        public void Delete(T entity)
+        {
+            Db.Remove(entity);
+            Save();
+        }
+
+        public IEnumerable<T> Find(Func<T, bool> predicate)
+        {
+            return Db.Set<T>().Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Db.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return Db.Set<T>().Find(id);
+        }
+
+
+
+        public void Update(T entity)
+        {
+            Db.Entry(entity).State = EntityState.Modified;
+            Save();
+        }
+
+        protected void Save()
+        {
+            Db.SaveChanges();
+        }
     }
 }
